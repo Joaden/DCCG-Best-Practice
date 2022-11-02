268d41943ea95f05c0c431b4cfe08d503a4585f2
DeniC <chanet.denis@hotmail.fr>
2022-11-02 23:43:36 +0100

    feat admin blog

--- src/DataFixtures/ProductsFixtures.php
+++ src/DataFixtures/ProductsFixtures.php
@@ -12,8 +12,43 @@ use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 //  implements FixtureGroupInterface
 
 
-class ProductsFixtures extends Fixture
+class ProductsFixtures extends Fixture implements FixtureGroupInterface
 {
+    
+    public function loadProducts(ObjectManager $manager)
+    {
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+
+        for ($i = 0; $i < 50; $i++) {
+            $product = new Products();
+            $product->setTitle($faker->productName)
+                ->setRef($faker->price(20, 200))
+                ->setEan($faker->price(20, 200))
+                ->setSlug($faker->price(20, 200))
+                ->setQte($faker->price(20, 200))
+                ->setQteLogistic($faker->price(20, 200))
+                ->setIsValid($faker->price(20, 200))
+                ->setIsShow($faker->price(20, 200))
+                // ->setDateStart($faker->price(20, 200))
+                // ->setDateEnd($faker->price(20, 200))
+                // ->setType($faker->price(20, 200))
+                // ->setSupplier($faker->price(20, 200))
+                // ->setTransportCode($faker->price(20, 200))
+                ->setDescription($faker->price(20, 200))
+                ->setPrice($faker->price(20, 200))
+                ->setSolde($faker->price(20, 200))
+                ->setCreatedAt($faker->price(20, 200));
+                // ->setImage($faker->imageUrl(400, 400))
+                // ->setStock($faker->stock(1, 100));
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+
     public function load(ObjectManager $manager)
     {
         // create 20 products! Bam!
@@ -25,5 +60,17 @@ class ProductsFixtures extends Fixture
         }
 
         $manager->flush();
+    }
+
+    
+    /**
+     * This method must return an array of groups
+     * on which the implementing class belongs to
+     *
+     * @return string[]
+     */
+    public static function getGroups(): array
+    {
+            return ['group1', 'group2'];
     }
 }
